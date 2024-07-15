2aeb2b35a2592d9a74f3830463357c35b3b46d49
Dibyadyuti Das <dibyadyutidas0@gmail.com>
2024-07-15 10:00:00 +0530

    Add Java methods and method overloading

---
+++ oop/method/method_.java
@@ -0,0 +1,16 @@
+public class method_ {
+    public static void main(String[] args){
+    
+        // *Method is a block of code that is executed when ever it open
+
+        String name = "Bro";
+        int age = 20;
+        hello(name,age);
+        // System.out.println();
+    }
+        
+    static void hello(String name,int age){
+        System.out.println("Hello "+name);
+        System.out.println("You are "+age+" year old");
+    }
+}
---
+++ oop/method/overloaded_m.java
@@ -0,0 +1,11 @@
+public class overloaded_m {
+    public static void main(String[] args){
+    
+        // *Overladed Method = Methods that share the same name but different parameters
+        // *                    Method name + parameter + method signature
+
+        // int x = 
+
+        // System.out.println();
+    }
+}
